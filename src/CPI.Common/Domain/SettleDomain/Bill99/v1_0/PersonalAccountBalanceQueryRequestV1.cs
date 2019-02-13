695e19a5c9e14b27d51292550694a24f8323c27e
苏锡波 <vsxibo@163.com>
2019-02-13 12:21:10 +0800

    Add cpi.settle.personal.accountbalance models.

---
+++ src/CPI.Common/Domain/SettleDomain/Bill99/v1_0/PersonalAccountBalanceQueryRequestV1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lotus.Validation;
+
+namespace CPI.Common.Domain.SettleDomain.Bill99.v1_0
+{
+    public class PersonalAccountBalanceQueryRequestV1 : ValidateModel
+    {
+    }
+}
---
+++ src/CPI.Common/Domain/SettleDomain/Bill99/v1_0/PersonalAccountBalanceQueryResponseV1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPI.Common.Domain.SettleDomain.Bill99.v1_0
+{
+    public class PersonalAccountBalanceQueryResponseV1 : CommonResponse
+    {
+    }
+}
