dce9ca99a8f8316fda8abb880f8abea5b006f5a0
苏锡波 <vsxibo@163.com>
2019-03-07 16:44:36 +0800

    Add ScheduleJob.

--- src/CPI.ScheduleJobs/AgreePay/YeePayAgreepayPayResultPullJob.cs
+++ src/CPI.ScheduleJobs/AgreePay/YeePayAgreepayPayResultPullJob.cs
@@ -6,14 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using CPI.ScheduleJobs.Models;
 using CPI.Utils;
-using Lotus.Logging;
-using Lotus.Net;
 using Lotus.Security;
-using Newtonsoft.Json;
+using Lotus.Net;
+using Lotus.Logging;
 
 namespace CPI.ScheduleJobs.AgreePay
 {
-    public class Bill99AgreepayPayResultPullJob : CPIJob
+    public class YeePayAgreepayPayResultPullJob : CPIJob
     {
         private static readonly ILogger _logger = LogManager.GetLogger();
 
@@ -29,7 +28,7 @@ namespace CPI.ScheduleJobs.AgreePay
             var postData = new
             {
                 CPIScheduleConfig.AppId,
-                Method = "cpi.unified.payresult.pull",
+                Method = "cpi.agreepay.payresult.pull.yeepay",
                 Version = "1.0",
                 Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
                 BizContent = JsonUtil.SerializeObject(bizContent).Value,
@@ -38,32 +37,32 @@ namespace CPI.ScheduleJobs.AgreePay
             };
 
             return _client.PostJsonAsync<CPIGatewayCommonResponse<CommonPullResult>>(CPIScheduleConfig.RequestUrl, JsonUtil.SerializeObject(postData).Value).ContinueWith(t0 =>
-             {
-                 if (t0.IsCompleted)
-                 {
-                     if (t0.IsCanceled || t0.IsFaulted)
-                     {
-                         Print("任务取消或失败");
-                         return;
-                     }
+            {
+                if (t0.IsCompleted)
+                {
+                    if (t0.IsCanceled || t0.IsFaulted)
+                    {
+                        Print("任务取消或失败");
+                        return;
+                    }
 
-                     var resp = t0.Result;
-                     if (!resp.Success)
-                     {
-                         _logger.Error("CPI.ScheduleJobs.AgreePay", "ERROR", $"{this.GetType().FullName}.Execute()", "_client.PostJson(...)", resp.ErrorMessage, resp.FirstException);
-                         Print(resp.ErrorMessage);
-                     }
-                     else
-                     {
-                         Print($"成功拉取 {resp.Value.Content.SuccessCount} 个结果");
-                     }
-                 }
-             });
+                    var resp = t0.Result;
+                    if (!resp.Success)
+                    {
+                        _logger.Error("CPI.ScheduleJobs.AgreePay", "ERROR", $"{this.GetType().FullName}.Execute()", "_client.PostJson(...)", resp.ErrorMessage, resp.FirstException);
+                        Print(resp.ErrorMessage);
+                    }
+                    else
+                    {
+                        Print($"成功拉取 {resp.Value.Content.SuccessCount} 个结果");
+                    }
+                }
+            });
         }
 
         private void Print(String content)
         {
-            Console.Out.WriteLineAsync($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]-[快钱协议支付]：{content}");
+            Console.Out.WriteLineAsync($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]-[易宝协议支付]：{content}");
         }
     }
 }
