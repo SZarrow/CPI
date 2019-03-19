253685b840c1f7791760bd37d242690ab745c72a
苏锡波 <007@dotnetws.com>
2019-03-19 15:56:29 +0800

    Add IDisposable for LockProvider.

--- src/CPI.Providers/LockProvider.cs
+++ src/CPI.Providers/LockProvider.cs
@@ -77,5 +77,40 @@ namespace CPI.Providers
                 }
             }
         }
+
+        public IDisposable CreateScope(Int32 key)
+        {
+            return new LockScope(this, key);
+        }
+
+        internal struct LockScope : IDisposable, IEquatable<LockScope>
+        {
+            private readonly LockProvider _provider;
+            private readonly Int32 _key;
+
+            public LockScope(LockProvider provider, Int32 key)
+            {
+                _provider = provider;
+                _key = key;
+
+                if (!_provider.Exists(_key))
+                {
+                    _provider.Lock(_key);
+                }
+            }
+
+            public void Dispose()
+            {
+                if (_provider.Exists(_key))
+                {
+                    _provider.UnLock(_key);
+                }
+            }
+
+            public Boolean Equals(LockScope other)
+            {
+                return this._key == other._key;
+            }
+        }
     }
 }
